6f3aa0cde94fd0f0d57ba79b2bdd15a948e7a497
Golfsimulator <Linus_oscarsson_1@hotmail.com>
2025-08-07 17:24:23 +0200

    Lägg till next.config.js

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+      {
+        source: '/robots.txt',
+        destination: '/api/robots',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
